b5948c8a0c7ef95bfed2f5f79cd555e6e492e31e
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-26 17:27:15 +0530

    FIX: Added all secrets to .env file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -18,10 +20,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
-app.secret_key = "jose"  # could do app.config['JWT_SECRET_KEY'] if we prefer
+app.secret_key = os.getenv(
+    "SECRET_KEY"
+)  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
 
 
