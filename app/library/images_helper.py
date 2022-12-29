427a737390347a2cae676e0e0b4e4ac872687fef
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-29 15:54:33 +0530

    BUILT: Added docs string in image_helper functions

--- app/library/images_helper.py
+++ app/library/images_helper.py
@@ -9,14 +9,23 @@ IMAGE_SET = UploadSet("images", IMAGES)
 
 
 def save_image(image: FileStorage, folder: str = None, name: str = None) -> str:
+    """
+    save the 'image' in the 'folder' with the given 'name'
+    """
     return IMAGE_SET.save(image, folder, name)
 
 
 def get_path(filename: str = None, folder: str = None) -> str:
+    """ "
+    return the full path of 'filename' from the 'folder'
+    """
     return IMAGE_SET.path(filename, folder)
 
 
 def find_image_any_format(filename: str = None, folder: str = None) -> Union[str, None]:
+    """
+    return the 'filename' from the 'folder' of any accpted extension from IMAGE
+    """
     for _format in IMAGES:
         image = f"{filename}.{_format}"
         image_path = IMAGE_SET.path(filename=image, folder=folder)
@@ -26,12 +35,18 @@ def find_image_any_format(filename: str = None, folder: str = None) -> Union[str
 
 
 def _retrieve_filename(file: Union[str, FileStorage]) -> str:
+    """
+    return the filename including filename.extension
+    """
     if isinstance(file, FileStorage):
         return file.filename
     return file
 
 
 def is_filename_safe(file: Union[str, FileStorage]) -> bool:
+    """
+    check for the valid filename.extension
+    """
     filename = _retrieve_filename(file)
 
     allowed_format = "|".join(IMAGES)
@@ -41,10 +56,16 @@ def is_filename_safe(file: Union[str, FileStorage]) -> bool:
 
 
 def get_basename(file: Union[str, FileStorage]) -> str:
+    """
+    return filename.extension of the 'file'(path/filename.extension)
+    """
     filename = _retrieve_filename(file)
     return os.path.split(filename)[1]
 
 
 def get_extension(file: Union[str, FileStorage]) -> str:
+    """
+    return the .extenison of the 'file'(path/filename.extension)
+    """
     filename = _retrieve_filename(file)
     return os.path.splitext(filename)[1]
