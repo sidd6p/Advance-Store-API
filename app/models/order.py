8b565ae09501b2d2a322e09b16a6e6a51eaac295
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-30 23:12:29 +0530

    BUILT: Added item_to_order linking table

--- app/models/order.py
+++ app/models/order.py
@@ -6,13 +6,21 @@ from app.db import db
 from app.models.item import ItemModel
 
 
+items_to_orders = db.Table(
+    "items_to_orders",
+    db.Column(db.Integer, primary_key=True),
+    db.Column("item_id", db.Integer, db.ForeignKey("item.id")),
+    db.Column("order_id", db.Integer, db.ForeignKey("orders.id")),
+)
+
+
 class OrderModel(db.Model):
     __tablename__ = "orders"
 
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(20), nullable=False)
 
-    items = db.relationship("ItemMode", lazy="dynamic")
+    items = db.relationship("ItemMode", secondary=items_to_orders, lazy="dynamic")
 
     @classmethod
     def find_by_id(cls, id: int) -> "OrderModel":
