cbbf2ebbea68538a295f7defd4da3f98d35275cf
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-30 17:02:30 +0530

    BUILT: Access of user data from github using oauth

--- app/resources/github_login.py
+++ app/resources/github_login.py
@@ -1,7 +1,11 @@
+import os
+
 from flask_restful import Resource
 from flask import g
 
 from app.oa import github
+from app.models.user import UserModel
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 
 class GithubLogin(Resource):
@@ -17,8 +21,21 @@ class GithubAuthorize(Resource):
     def get(cls):
         resp = github.authorized_response()
         g.access_token = resp["access_token"]
-        github_user = github.get(
-            "user"
-        )  # this uses the access_token from the tokengetter function
-        github_username = github_user.data["login"]
-        return github_username
+        github_user = github.get("user")
+
+        username = github_user.data["login"]
+        email = github_user.data["email"]
+        user = UserModel.find_by_username(username)
+
+        if not user:
+            user = UserModel(
+                username=username,
+                email=email,
+                password=os.environ.get("RANDOM_PASSWORD", "28907SDF34"),
+            )
+            user.save_to_db()
+
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
