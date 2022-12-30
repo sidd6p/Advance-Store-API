67781339f2853605d7ce32bf782e206420787136
Siddhartha Purwar <siddpurwar@gmail.com>
2022-12-31 00:30:43 +0530

    BUILT: Added new ItemInOrder model

--- app/models/order.py
+++ app/models/order.py
@@ -6,12 +6,23 @@ from app.db import db
 from app.models.item import ItemModel
 
 
-items_to_orders = db.Table(
-    "items_to_orders",
-    db.Column(db.Integer, primary_key=True),
-    db.Column("item_id", db.Integer, db.ForeignKey("item.id")),
-    db.Column("order_id", db.Integer, db.ForeignKey("orders.id")),
-)
+# items_to_orders = db.Table(
+#     "items_to_orders",
+#     db.Column(db.Integer, primary_key=True),
+#     db.Column("item_id", db.Integer, db.ForeignKey("item.id")),
+#     db.Column("order_id", db.Integer, db.ForeignKey("orders.id")),
+# )
+
+
+class ItemsInOrders(db.Model):
+    __tablename__ = "items_in_order"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey("item.id"))
+    order_id = db.Column(db.Integer, db.ForeignKey("item.id"))
+
+    item = db.relationship("ItemModel")
+    order = db.relationship("OrderModel", back_populates="items")
 
 
 class OrderModel(db.Model):
@@ -20,7 +31,7 @@ class OrderModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(20), nullable=False)
 
-    items = db.relationship("ItemMode", secondary=items_to_orders, lazy="dynamic")
+    items = db.relationship("ItemsInOrders", back_populates="order")
 
     @classmethod
     def find_by_id(cls, id: int) -> "OrderModel":
